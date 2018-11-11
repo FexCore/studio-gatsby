40577b5c7f2a4889a524432d7fee9c54d9d13ef1
Amer Hidanovic <amer.hidanovic@gmail.com>
2018-11-11 15:57:25 +0100

    Done with latest projects

--- src/components/HomePage/HomeFooter/HomeFooter.js
+++ src/components/HomePage/HomeFooter/HomeFooter.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import Container from '../../Shared/Container/Container'
 import LatestProjects from '../../Shared/LatestProjects'
 import styles from './HomeFooter.module.scss';
 
 const HomeFooter = () => {
     return(
         <div className={styles.footer}>
-            <LatestProjects />
+            <Container>
+                <LatestProjects />
+            </Container>
         </div>
     )
 }
